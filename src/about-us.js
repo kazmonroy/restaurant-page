b8f289875daaaacb3d05b70600346e196be3ae48
kazmonroy <katherinemonroy@gmail.com>
2022-09-22 00:06:17 +0200

    Fix about us pictures

--- src/about-us.js
+++ src/about-us.js
@@ -12,13 +12,13 @@ function createAboutUsCards() {
   const cardAboutUs = createCard(
     "About Us",
     "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam?",
-    "/src/img/sushi-1.jpg"
+    "https://images.unsplash.com/photo-1612203985758-5f8453e91056?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
   );
 
   const cardOurMission = createCard(
     "Our mission",
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, numquam!",
-    "/src/img/sushi-2.jpg"
+    "https://images.unsplash.com/photo-1603792451785-e2c78de442a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3174&q=80"
   );
 
   cardOurMission.classList.add("reverse");
